867dd5d8ae05e1f57addc0ed07770fd209613728
Pedro G <otherview@gmail.com>
2018-02-06 14:21:42 -0800

    Added some bits around eslint :)

--- src/Tests/Integration/AuthControllerTests.js
+++ src/Tests/Integration/AuthControllerTests.js
@@ -6,18 +6,17 @@ chai.use(chaiHttp);
 const expect = chai.expect; // we are using the "expect" style of Chai
 
 describe('Basic Authentication tests', () => {
-
   let tokenType;
   let accessToken;
   let refreshToken;
   let expiryToken;
-  let AuthHeader = {'Authorization':tokenType + ' ' + accessToken};
+  const AuthHeader = { Authorization: `${tokenType} ${accessToken}` };
 
-  it('Should be able to auth to get a JWT', function (done) {
+  it('Should be able to auth to get a JWT', (done) => {
     chai.request(MockConfigs.Url)
       .post('/v1/auth/login')
-      .send({"email": "derp@derpmail.com", "password": "derp2"})
-      .end(function (err, result) {
+      .send({ email: 'derp@derpmail.com', password: 'derp2' })
+      .end((err, result) => {
         expect(result).to.have.status(200);
         console.log(result.body);
 
@@ -28,6 +27,4 @@ describe('Basic Authentication tests', () => {
         done();
       });
   });
-
-
 });
--- src/Tests/Integration/UserControllerTests.js
+++ src/Tests/Integration/UserControllerTests.js
@@ -1,25 +1,24 @@
 const chai = require('chai');
 const chaiHttp = require('chai-http');
 const httpStatus = require('http-status');
-chai.use(chaiHttp);
-const expect = chai.expect; // we are using the "expect" style of Chai
-
 const MockConfigs = require('../assets/Configs');
 
+const expect = chai.expect;
+chai.use(chaiHttp);
 
 describe('Basic User Tests - Create, List, Get, Delete', () => {
+  const closure = {};
 
-  let closure = {};
-
-  it('Auth: Should be able to auth to get a JWT', function (done) {
+  it('Auth: Should be able to auth to get a JWT', (done) => {
     chai.request(MockConfigs.Url)
       .post('/v1/auth/login')
-      .send({"email": "derp@derpmail.com", "password": "derp2"})
-      .end(function (err, result) {
+      .send({ email: 'derp@derpmail.com', password: 'derp2' })
+      .end((err, result) => {
         expect(result).to.have.status(200);
 
         closure.tokenType = result.body.token.tokenType;
         closure.accessToken = result.body.token.accessToken;
+        closure.authToken = `${closure.tokenType} ${closure.accessToken}`;
         closure.refreshToken = result.body.token.refreshToken;
         closure.expiryToken = result.body.token.expiryToken;
 
@@ -30,8 +29,8 @@ describe('Basic User Tests - Create, List, Get, Delete', () => {
   it('User.List: Should be able to list Users', function (done) {
     chai.request(MockConfigs.Url)
       .get('/v1/users')
-      .set('Authorization',closure.tokenType + ' ' + closure.accessToken)
-      .end(function (err, result) {
+      .set('Authorization', closure.authToken)
+      .end((err, result) => {
         expect(result).to.have.status(200);
 
         done();
@@ -41,8 +40,8 @@ describe('Basic User Tests - Create, List, Get, Delete', () => {
   it('User.Create: Should be able to create a new user', function (done) {
     chai.request(MockConfigs.Url)
       .post('/v1/users')
-      .set('Authorization',closure.tokenType + ' ' + closure.accessToken)
-      .send({"email": "newUser@gmail.com", "password": "stuff", "role":"admin"})
+      .set('Authorization', closure.authToken)
+      .send({email: 'newUser@gmail.com', password: 'stuff', role:'admin'})
       .end(function (err, result) {
         expect(result).to.have.status(httpStatus.OK);
         closure.CreatedUserId = result.body.id;
@@ -51,12 +50,13 @@ describe('Basic User Tests - Create, List, Get, Delete', () => {
   });
 
   it('User.Get: Should be able to Get the newly created a new user', function (done) {
+    const requestUrl = `/v1/users/${closure.CreatedUserId}`;
     chai.request(MockConfigs.Url)
-      .get('/v1/users/' + closure.CreatedUserId)
-      .set('Authorization',closure.tokenType + ' ' + closure.accessToken)
+      .get(requestUrl)
+      .set('Authorization', closure.authToken)
       .end(function (err, result) {
         expect(result).to.have.status(httpStatus.OK);
-        expect(result.body.email).to.have.email("newUser@gmail.com")
+        expect(result.body.email).to.have.email('newUser@gmail.com')
         closure.CreatedUserId = result.body.id;
         done();
       });
@@ -65,9 +65,9 @@ describe('Basic User Tests - Create, List, Get, Delete', () => {
   it('User.Create: Should NOT be able to create a new user with ID', function (done) {
     chai.request(MockConfigs.Url)
       .post('/v1/users')
-      .set('Authorization',closure.tokenType + ' ' + closure.accessToken)
-      .send({"id":closure.CreatedUserId, "email": "newUser@gmail.com", "password": "stuff", "role":"admin"})
-      .end(function (err, result) {
+      .set('Authorization', closure.authToken)
+      .send({id: closure.CreatedUserId, email: 'newUser@gmail.com', password: 'stuff', role:'admin'})
+      .end((err, result) => {
         expect(result).to.have.status(httpStatus.UNAUTHORIZED);
 
         done();
@@ -77,9 +77,9 @@ describe('Basic User Tests - Create, List, Get, Delete', () => {
   it('User.Create: Should NOT be able to create a new user with the same email', function (done) {
     chai.request(MockConfigs.Url)
       .post('/v1/users')
-      .set('Authorization',closure.tokenType + ' ' + closure.accessToken)
-      .send({"email": "newUser@gmail.com", "password": "stuff", "role":"admin"})
-      .end(function (err, result) {
+      .set('Authorization', closure.authToken)
+      .send({email: 'newUser@gmail.com', password: 'stuff', role:'admin'})
+      .end((err, result) => {
         expect(result).to.have.status(httpStatus.UNAUTHORIZED);
 
         done();
@@ -87,11 +87,12 @@ describe('Basic User Tests - Create, List, Get, Delete', () => {
   });
 
   it('User.Delete: Should be able to delete a user using the Id', function (done) {
-    chai.request(MockConfigs.Url)
-      .delete('/v1/users/' + closure.CreatedUserId)
-      .set('Authorization',closure.tokenType + ' ' + closure.accessToken)
+    const requestUrl = `/v1/users/${closure.CreatedUserId}`;
 
-      .end(function (err, result) {
+    chai.request(MockConfigs.Url)
+      .delete(requestUrl)
+      .set('Authorization', closure.authToken)
+      .end((err, result) => {
         expect(result).to.have.status(httpStatus.OK);
 
         done();
@@ -99,11 +100,12 @@ describe('Basic User Tests - Create, List, Get, Delete', () => {
   });
 
   it('User.Delete: Should NOT be able to delete a non existing user using the Id', function (done) {
-    chai.request(MockConfigs.Url)
-      .delete('/v1/users/' + closure.CreatedUserId)
-      .set('Authorization',closure.tokenType + ' ' + closure.accessToken)
+    const requestUrl = `/v1/users/${closure.CreatedUserId}`;
 
-      .end(function (err, result) {
+    chai.request(MockConfigs.Url)
+      .delete(requestUrl)
+      .set('Authorization', closure.authToken)
+      .end((err, result) => {
         expect(result).to.have.status(httpStatus.UNAUTHORIZED);
 
         done();
@@ -113,9 +115,9 @@ describe('Basic User Tests - Create, List, Get, Delete', () => {
   it('User.Delete: Should fail if no Id provided', function (done) {
     chai.request(MockConfigs.Url)
       .delete('/v1/users/ ')
-      .set('Authorization',closure.tokenType + ' ' + closure.accessToken)
+      .set('Authorization', closure.authToken)
 
-      .end(function (err, result) {
+      .end((err, result) => {
         expect(result).to.have.status(httpStatus.BAD_REQUEST);
 
         done();
